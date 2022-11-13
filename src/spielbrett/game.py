bdb6e7fb95b081e27e3b54d6cf742d97be4e0ac4
Oleg Evseev <git@x3n.me>
2022-11-13 23:04:21 +0400

    Add docstring to the observe method

--- src/spielbrett/game.py
+++ src/spielbrett/game.py
@@ -8,6 +8,11 @@ class Game:
     """Base class for Project Spielbrett games."""
 
     def observe(self, player_index: PlayerIndex) -> Dict[str, Any]:
+        """
+        Returns a dictionary containing all the properties observable
+        by the provided user.
+        """
+
         return {
             observation_name: self.__getattribute__(observation_name)(player_index)
             for observation_name in _observations[self.__class__.__name__]
